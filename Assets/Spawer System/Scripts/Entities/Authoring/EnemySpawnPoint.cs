fa583b794a97a2f7bba3b56f31219d39de736266
The Dreamer <dreamers.inc12@gmail.com>
2020-04-28 20:27:18 -0400

    Refactor Add Generate Authoriing

--- Assets/Spawer System/Scripts/Entities/Authoring/EnemySpawnPoint.cs
+++ Assets/Spawer System/Scripts/Entities/Authoring/EnemySpawnPoint.cs
@@ -10,19 +10,6 @@ namespace SpawnerSystem
         public int MaxEnemyLevel;
 
         public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
-        {
-            var Spawnpoint = new SpawnPointComponent() { SpawnPointID= SpawnPointID};
-            dstManager.AddComponentData(entity, Spawnpoint);
-            var EnemySpawn = new EnemySpawnTag() { MaxLevel = MaxEnemyLevel };
-            dstManager.AddComponentData(entity, EnemySpawn);
-            DynamicBuffer<EnemySpawnData> buffer =  dstManager.AddBuffer<EnemySpawnData>(entity);
-
-            foreach (int SpawnID in SpawnIDList)
-            {
-                buffer.Add(new EnemySpawnData() { SpawnID = SpawnID});
-            }
-
-        }
-
+        { }
     }
 }
